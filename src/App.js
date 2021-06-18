576a165602a7e645ec1f8c26cd6bdd7751f2301d
ndrohith09 <ndrohith09@gmail.com>
2021-06-18 20:13:55 +0530

    startpage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {Route , Switch , BrowserRouter as Router } from 'react-router-dom';
 import Instructionpage from './components/Instructionpage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button , Jumbotron, Container } from 'react-bootstrap';
+import Img  from './components/images/startpage.jpeg'; 
 import Quiz from './components/Quiz1';
 import Quiz2 from './components/Quiz2';
 import Level2 from './components/Level2';
@@ -13,8 +15,16 @@ function App() {
       <Switch>
         <Router exact path ="/">
         <div className="App">
-
-    hello
+	<Jumbotron fluid>
+	  <Container>
+	    <h1>Fluid jumbotron</h1>
+	    <p>
+	      This is a modified jumbotron that occupies the entire horizontal space of
+	      its parent.
+	    </p>
+	<img src={Img} alt="image" />
+	  </Container>
+	</Jumbotron>
     <a href="instruction1"> Start the War</a>
         </div>
     </Router>
